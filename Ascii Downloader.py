5587f4580b378fe2640868c0f037e0d3eb5ab3e8
Marcogamer88 <68127218+Marcogamer88@users.noreply.github.com>
2020-08-20 17:05:14 +0200

    Python File
    The Python File of Ascii Downloader

---
+++ Ascii Downloader.py
@@ -0,0 +1,43 @@
+import tkinter as tk
+import requests
+
+window = tk.Tk()
+window.geometry("900x550")
+window.title("ASCII Art Downloader By Marco")
+window.grid_columnconfigure(0, weight=1)
+
+
+def download_ascii():
+    if text_input.get():
+        user_input = text_input.get()
+        payload = {"text": user_input}
+        ascii_art = requests.get("http://artii.herokuapp.com/make",
+                                 params=payload)
+        text_response = ascii_art.text
+    else:
+        text_response = "Aggiungi una parola o una frase!"
+
+    textwidget = tk.Text()
+    textwidget.insert(tk.END, text_response)
+    textwidget.grid(row=3, column=0, sticky="WE", padx=10, pady=10)
+
+    credits_label = tk.Label(window, text="Created by Marco, ASCII Art Powered by artii.herokuapp.com")
+    credits_label.grid(row=4, column=0, sticky="S", pady=10)
+
+
+
+welcome_label = tk.Label(window,
+                         text="Benvenuto! Aggiungi una parola o una frase da scaricare:",
+                         font=("Helvetica", 15))
+welcome_label.grid(row=0, column=0, sticky="N", padx=20, pady=10)
+
+
+text_input = tk.Entry()
+text_input.grid(row=1, column=0, sticky="WE", padx=10)
+
+download_button = tk.Button(text="Clicca qui per scaricare il tuo ASCII", command=download_ascii)
+download_button.grid(row=2, column=0, sticky="WE", pady=10, padx=10)
+
+
+if __name__ == "__main__":
+    window.mainloop()
